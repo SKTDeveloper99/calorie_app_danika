8e4e8fbdf0c3cfea972111925769133c41893816
cooldanika06 <danikayiwenmei@gmail.com>
2023-08-13 11:13:44 -0700

    update firebase database

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -79,3 +79,85 @@
 //     );
 //   }
 // }
+
+import 'package:calorie_app_danika/form_widgets.dart';
+import 'package:calorie_app_danika/health_log.dart';
+import 'package:calorie_app_danika/profile_page.dart';
+import 'package:flutter/material.dart';
+
+/// Flutter code sample for [BottomNavigationBar].
+
+class BottomNavigationBarPage extends StatefulWidget {
+  const BottomNavigationBarPage({super.key});
+
+  @override
+  State<BottomNavigationBarPage> createState() =>
+      _BottomNavigationBarPageState();
+}
+
+class _BottomNavigationBarPageState
+    extends State<BottomNavigationBarPage> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static List<Widget> _widgetOptions = <Widget>[
+    ProfilePage(),
+    FormWidgetsDemo(),
+    LogScreen(),
+    Text(
+      'Index 3: Settings',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 4: New Page',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+            backgroundColor: Colors.amberAccent,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.format_align_center),
+            label: 'Form',
+            backgroundColor: Colors.amberAccent,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_chart_rounded),
+            label: 'Health Log',
+            backgroundColor: Colors.amberAccent,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+            backgroundColor: Colors.amberAccent,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'New Page',
+            backgroundColor: Colors.amberAccent,
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
